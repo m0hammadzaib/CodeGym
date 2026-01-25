7aa2ba41155833fe53d1a18cebffa0fca7f06c32
m0hammadzaib <zaibfr4@gmail.com>
2026-01-25 16:20:37 +0530

    MiddleWares in Express

--- Backend/film90/main.js
+++ Backend/film90/main.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
+app.use((req,res,next)=>{
+  console.log("Logged In");
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello Express World!')
+})
+app.get('/about', (req, res) => {
+  res.send('Hello About!')
+})
+app.get('/contact', (req, res) => {
+  res.send('Hello Contact!')
 })
 
 app.listen(port, () => {
