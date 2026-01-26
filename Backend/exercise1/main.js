96b25eb38938ba95046127b452a695de4ee5258f
m0hammadzaib <zaibfr4@gmail.com>
2026-01-26 11:36:48 +0530

    EJS Template Engine

--- Backend/exercise1/main.js
+++ Backend/exercise1/main.js
@@ -1,33 +1,36 @@
 const fs = require("fs");
 const path = require("path");
 
-// SOURCE FOLDER (jahan files hain)
+// SOURCE FOLDER
 const sourceDir = "Z:\\CodeGym\\Backend\\exercise1";
 
-fs.readdirSync(sourceDir).array.forEach(e => {
-     const oldPath = path.join(sourceDir, e);
-    if(!fs.existsSync(sourceDir)){
-        if(path.extname(e)=== ".txt"){
-            fs.mkdir(sourceDir,"Text")
-            const newFolder = path.join(sourceDir,"Text")
-             const newPath = path.join(newFolder, e);
-             fs.rename(oldPath,newPath)
-        }
-        else if(path.extname(e)=== ".pdf"){
-            fs.mkdir(sourceDir,"PDF");
-            const newFolder2 = path.join(sourceDir,"PDF")
-             const newPath = path.join(newFolder2, e);
-             fs.rename(oldPath,newPath)
-        }
-        else if(path.extname(e)=== ".png" && ".jpg"){
-            fs.mkdir(sourceDir,"Photos");
-            const newFolder3 = path.join(sourceDir,"Photos")
-             const newPath = path.join(newFolder3, e);
-             fs.rename(oldPath,newPath)
-        }
+// Read all files
+fs.readdirSync(sourceDir).forEach(e => {
+    const ext = path.extname(e).toLowerCase(); // lowercase to handle .JPG etc
+    const oldPath = path.join(sourceDir, e);
+
+    if (ext === ".txt") {
+        const textFolder = path.join(sourceDir, "Text");
+        if (!fs.existsSync(textFolder)) fs.mkdirSync(textFolder);
+        const newPath = path.join(textFolder, e);
+        fs.renameSync(oldPath, newPath);
     }
-    
+    else if (ext === ".pdf") {
+        const pdfFolder = path.join(sourceDir, "PDF");
+        if (!fs.existsSync(pdfFolder)) fs.mkdirSync(pdfFolder);
+        const newPath = path.join(pdfFolder, e);
+        fs.renameSync(oldPath, newPath);
+    }
+    else if (ext === ".png" || ext === ".jpg") {
+        const photoFolder = path.join(sourceDir, "Photos");
+        if (!fs.existsSync(photoFolder)) fs.mkdirSync(photoFolder);
+        const newPath = path.join(photoFolder, e);
+        fs.renameSync(oldPath, newPath);
+    }
 });
+
+console.log("Files moved successfully ✅");
+
 
 
 
---
+++ Backend/film92/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.send("Hello Express")
+})
+app.get('/nav',(req,res)=>{
+    res.sendFile('templates/index.html',{root:__dirname})
+})
+
+app.listen(port,()=>{
+console.log(`Express app listening on port ${port}`)
+})
