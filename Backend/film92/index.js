d91d91b1a9352e9caf8d38063f9ebbc5b7aa5f51
m0hammadzaib <zaibfr4@gmail.com>
2026-01-29 10:46:30 +0530

    postgreSQL

--- Backend/film92/index.js
+++ Backend/film92/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("views", "ejs"); 
+
 app.get('/',(req,res)=>{
     let siteName = "BootStrap"
     let searchText = "SearchNow"
---
+++ postgreSQL/practice1.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM users WHERE role IN ('admin','manager';
+
+-- SELECT skills FROM users WHERE skillS IS NULL;
+-- SELECT COUNT (*) FROM users WHERE skills IS NULL'
+-- SELECT COUNT (*) FROM users WHERE salary BETWEEN 20000 AND 50000;
+-- SELECT COUNT (*) FROM users WHERE role = 'admin' AND age <30 ;
+-- SELECT * FROM users WHERE role = 'admin' OR role ='manager' ;
+
+
