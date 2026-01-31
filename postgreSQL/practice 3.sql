154ba2575498d1ec2285c65e1d6e5cc64e131e25
m0hammadzaib <zaibfr4@gmail.com>
2026-01-31 21:44:31 +0530

    GROUP BY Function

--- postgreSQL/practice 3.sql
+++ postgreSQL/practice 3.sql
@@ -1,4 +1,17 @@
+
 -- SELECT * FROM users
 -- WHERE role IN ('user','admin')
-SELECT * FROM users WHERE email LIKE ('Z','A')
+-- SELECT * FROM users WHERE username ILIKE ('Z%')
+
+-- SELECT * FROM users WHERE username LIKE ('za%')
+
+-- SELECT gender, SUM(salary)  FROM users GROUP BY gender;
+
+-- SELECT role, SUM(rating) FROM users GROUP BY role;
+-- SELECT role, SUM(rating) FROM users GROUP BY role;
+
+-- SELECT DATE(created_at),COUNT(username) FROM users GROUP BY DATE(created_at);
+
+
+
 
