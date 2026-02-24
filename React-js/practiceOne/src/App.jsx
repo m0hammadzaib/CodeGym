21d6181e4f20cd1af9cb94986de74ea02ca92952
m0hammadzaib <zaibfr4@gmail.com>
2026-02-24 19:06:01 +0530

    Added React-js folder for practicing react-js

---
+++ React-js/practiceOne/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-olive-900 w-screen h-screen flex justify-center items-center'>
+      <div className='text-red-700 text-3xl font-extrabold'>Tailwind is working!!!</div>
+    </div>
+  )
+}
+
+export default App
