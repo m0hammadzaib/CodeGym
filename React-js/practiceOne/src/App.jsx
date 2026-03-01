c95e92a2bd572752a609f32f31e31dc40986748f
m0hammadzaib <zaibfr4@gmail.com>
2026-03-01 16:09:39 +0530

    Practicing CRUD

--- React-js/practiceOne/src/App.jsx
+++ React-js/practiceOne/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 
+const noteArr = [
+    { id: 1, title: "First" },
+  { id: 2, title: "Second" },
+  { id: 3, title: "Third" },
+  { id: 3, title: "One bro" },
+  { id: 3, title: "three bro" },
+  { id: 3, title: "go bro and not no" },
+]
+
 const App = () => {
-  const [notes, setnotes] = useState([]);
+  const [notes, setnotes] = useState([
+  { id: 1, title: "First" },
+  { id: 2, title: "Second" },
+  { id: 3, title: "Third" },
+   { id: 3, title: "One bro" },
+  { id: 3, title: "three bro" },
+  { id: 3, title: "go bro and not no" },
+]
+);
 
   const addNote =()=>{
   const newNote={
@@ -23,8 +40,8 @@ const oldArr = [
 // const newArr = [...oldArr]; shallow copy 
 // newArr[0].name = "rahul";
 // console.log(oldArr);
-const deepCopy = JSON.parse(JSON.stringify(oldArr));
-deepCopy[0].name = "Ali";
+// const deepCopy = JSON.parse(JSON.stringify(oldArr));
+// deepCopy[0].name = "Ali";
 
 console.log(oldArr);
 
@@ -35,10 +52,34 @@ useEffect(() => {
     console.log("Updated Notes:", notes);
   }, [notes]);
 
+  const delteNotes=(id)=>{
+    // setnotes(prev => prev.filter(note => note.id !== id)); filter method
+  }
+  const removeNote =(id)=>{
+ setnotes(prev=>{
+   const newNote =[...prev];
+   const index = newNote.findIndex(n=>n.id ===id);
+   if(index !=-1){
+    newNote.splice(index,1)
+   }
+   return newNote;
+ }
+ )};
+
+
   return (
     <div className='flex justify-center items-center w-screen h-screen bg-olive-700'>
       <button className='bg-white text-black font-extrabold px-3 py-3 rounded-2xl' onClick={addNote}>Add</button>
+    {notes.map(n=>(
+      <div className='flex flex-col justify-center items-center'>
+      <div className='font-bold bg-red-600 p-5' key={n.id}>{n.title}
+      <button  className='cursor-pointer bg-black text-white p-2 rounded-2xl'onClick={()=>removeNote(n.id)}>Delete</button>
+      </div>
+      </div>
+
+    ))}
     </div>
+
   )
 }
 
